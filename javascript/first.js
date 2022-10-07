d51e657d7f0bb4f489056b7412f18701eca635af
Bapt1917 <stawlker80@gmail.com>
2022-10-07 16:40:46 +0200

    test Users

--- javascript/first.js
+++ javascript/first.js
@@ -6,3 +6,21 @@ sur plusieurs ligne */
 alert ('Bonjour, je suis UTRON et je fais bugger ton site'); 
 //texte dans la console
 console.log ('Texte en console');
+
+// Création de la promesse
+const promoseGetUsers = new Promise(function(resolve, reject) {
+    if(USER!== 'undifined') {
+        resolve (USER);
+    } else { 
+        reject ('Accès impossible, libérez Popo !');
+    }
+});
+
+//Récupérer l'état de la promesse
+promoseGetUsers
+.then(function(u){
+    console.log(u);
+})
+.catch(function(u){
+        console.log(u);
+});
