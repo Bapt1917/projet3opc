3d77b84c0bfe2a2f5388b8c9c2bb7605f20a390c
Bapt1917 <stawlker80@gmail.com>
2022-10-13 18:19:35 +0200

    Update first.js

--- javascript/first.js
+++ javascript/first.js
@@ -3,7 +3,7 @@
 /* Je créé plusieurs ligne de commentaire
 sur plusieurs ligne */
 
-alert ('Bonjour, je suis UTRON et je fais bugger ton site'); 
+/*alert ('Bonjour, je suis UTRON et je fais bugger ton site'); 
 //texte dans la console
 console.log ('Texte en console');
 
